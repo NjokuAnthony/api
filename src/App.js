08aba03f9edbef276733b195e15c45405dfb0176
NjokuAnthony <a_njokuchibuzor@yahoo.com>
2023-05-05 14:39:01 +0100

    fecting api

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState, useEffect} from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [users, setUsers] = useState([])
+
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => setUsers(response.data))
+    .catch(error => console.log(error) )
+  }, [])
+  return (
+    <div>
+      <table border='1'>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>USERNAME</td>
+            <td>EMAIL</td>
+            <td>ADDRESS</td>
+            <td>GEO</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((users, index) => (
+            <tr key={index}>
+              <td>{users.id}</td>
+              <td>{users.name}</td>
+              <td>{users.username}</td>
+              <td>{users.email}</td>
+              <td>{users.address.street},  {users.address.suite},  {users.address.city},  {users.address.zipcode}</td>
+              {/* <td>{users.geo[0]}, {users.geo.}</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default App
